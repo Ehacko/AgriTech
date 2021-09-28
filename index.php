77839ef150c8498672a875cc31eeedf113cf1472
Ehacko <49614514+Ehacko@users.noreply.github.com>
2021-09-28 19:38:35 -0400

    Update index.php

--- index.php
+++ index.php
@@ -7,9 +7,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="AgriTech est une entreprise de construction d'engin agricole">
+    <meta property="og:title" content="Title Here"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:url" content="/"/>
+    <meta property="og:image" content="/imgs/snippet.jpg"/>
+    <meta property="og:description" content="AgriTech est une entreprise de construction d'engin agricole"/>
     <meta http-equiv="Content-Security-Policy" content="
       default-src 'self';
-      img-src * data:;
+      img-src  https://*;
       child-src 'none';
       object-src 'none';
     ">
