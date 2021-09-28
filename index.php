46d273a3b8fe4cf0556a17598080bdf4775d1973
Ehacko <49614514+Ehacko@users.noreply.github.com>
2021-09-28 18:42:20 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php 
-#include 'components/db.php' 
- var_dump(curl 'https://api.airtable.com/v0/appOuRReYKFkgUUJf/contact?api_key=YOUR_API_KEY')
+#include 'components/db.php'
+$ch = curl_init("'https://api.airtable.com/'");
+$fp = fopen("v0/appOuRReYKFkgUUJf/contact?api_key=YOUR_API_KEY", "w");
+
+curl_setopt($ch, CURLOPT_FILE, $fp);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+
+curl_exec($ch);
+if(curl_error($ch)) {
+    fwrite($fp, curl_error($ch));
+}
+curl_close($ch);
+fclose($fp);
 ?>
 <!DOCTYPE html>
 <html lang="fr">
