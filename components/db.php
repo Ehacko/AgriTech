0189f95f91e316dc321bd4d6798b911d57f3760e
Ehacko <49614514+Ehacko@users.noreply.github.com>
2021-09-29 14:54:24 -0400

    add postgre

--- components/db.php
+++ components/db.php
@@ -1,4 +1,13 @@
 <?php
-  $db = new PDO('mysql:host=localhost; dbname=agritech', 'root', '');
-  $db -> exec("SET CHARACTER SET utf8");
+  #$db = new PDO('mysql:host=localhost; dbname=agritech', 'root', '');
+  #$db -> exec("SET CHARACTER SET utf8");
+  $dbi = parse_url(getenv("DATABASE_URL"));
+  $db = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $dbi["host"],
+    $dbi["port"],
+    $dbi["user"],
+    $dbi["pass"],
+    ltrim($dbi["path"], "/")
+));
 ?>
