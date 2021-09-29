33e557ffaa5726f7e39ccd027ab00d8940656820
Ehacko <49614514+Ehacko@users.noreply.github.com>
2021-09-29 15:34:35 -0400

    Update header.php

--- components/header.php
+++ components/header.php
@@ -1,7 +1,7 @@
 <header id="home" class="text-light d-grid align-content-middle">
   <div>
     <div class="rounded_img">
-      <img src="logo.avif" alt="Logo"/>
+      <img src="/imgs/logo.avif" alt="Logo"/>
     </div>
     <h1>AgriTech</h1>
   </div>
