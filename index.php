7b19eb7164bb3ec94ea0fa7a445a4cd460fcf9e5
Ehacko <49614514+Ehacko@users.noreply.github.com>
2021-09-28 23:36:34 -0400

    Update index.php

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
       <header id="home" class="text-light d-grid align-content-middle">
         <div>
           <div class="rounded_img">
-            <img src="https://img.pixers.pics/pho_wat(s3:700/FO/65/76/31/34/700_FO65763134_cff002fd772d21242e831c74c9f6cde2.jpg,700,699,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,481,650,jpg)/papiers-peints-icone-moissonneuse-batteuse-d-39-epis-de-ble.jpg.jpg" alt="Logo"></div>
+            <img src="/imgs/logo.avif" alt="Logo"></div>
             <h1>AgriTech</h1>
           </div>
       </header>
