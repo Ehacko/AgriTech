6e72d93f1c6d78ece17a790a478f1fc160037ad7
Ehacko <49614514+Ehacko@users.noreply.github.com>
2021-09-29 15:56:29 -0400

    Update head.php

--- components/head.php
+++ components/head.php
@@ -5,7 +5,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="AgriTech est une entreprise de construction d'engin agricole">
-<meta property="og:title" content="Title Here"/>
+<meta property="og:title" content="AgriTech"/>
 <meta property="og:type" content="website"/>
 <meta property="og:url" content="/"/>
 <meta property="og:image" content="/imgs/preview.avif"/>
