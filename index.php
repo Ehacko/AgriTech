594acdd0f8886d6a71a8b13d2d654bfe57830e34
Ehacko <49614514+Ehacko@users.noreply.github.com>
2021-09-29 22:13:57 -0400

    Update index.php

--- index.php
+++ index.php
@@ -5,29 +5,29 @@
 //   header("Expires: $ts");
 //   header("Pragma: cache");
 //   header("Cache-Control: max-age=$seconds_to_cache");
-//   include '/cache/top-cache.php'
-  include '/components/db.php'
+  include 'cache/top-cache.php'
+  include 'components/db.php'
 ?>
 <!DOCTYPE html>
 <html lang="fr">
   <head>
-    <?php include '/components/head.php' ?>
+    <?php include 'components/head.php' ?>
   </head>
   <body>
     <main>
-      <?php include '/components/header.php' ?>
-      <?php include '/components/nav.php' ?>
+      <?php include 'components/header.php' ?>
+      <?php include 'components/nav.php' ?>
       <div class="main">
-        <?php include '/components/about.php' ?>
-        <?php include '/components/machines.php' ?>
-        <?php include '/components/media.php' ?>
+        <?php include 'components/about.php' ?>
+        <?php include 'components/machines.php' ?>
+        <?php include 'components/media.php' ?>
       </div>
-      <?php include '/components/footer.php' ?>
+      <?php include 'components/footer.php' ?>
     </main>
   </body>
 </html>
 
 <?php
   #$query_str = null; $db_select = null 
-//   include '/cache/bottom-cache.php'
+  include 'cache/bottom-cache.php'
 ?>
