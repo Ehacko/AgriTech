d1e51a8dd147a1475a7859ca65226167328f7391
Ehacko <49614514+Ehacko@users.noreply.github.com>
2021-09-29 22:17:47 -0400

    Update bottom-cache.php

--- cache/bottom-cache.php
+++ cache/bottom-cache.php
@@ -1,7 +1,8 @@
 <?php
+echo 'bottom script';
 // Cache the contents to a cache file
-$cached = fopen($cachefile, 'w');
-fwrite($cached, ob_get_contents());
-fclose($cached);
-ob_end_flush(); // Send the output to the browser
+// $cached = fopen($cachefile, 'w');
+// fwrite($cached, ob_get_contents());
+// fclose($cached);
+// ob_end_flush(); // Send the output to the browser
 ?>
