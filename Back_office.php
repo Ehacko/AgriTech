45433adad9feac81c0d1a2cea11645d51adb0e9f
Ehacko <49614514+Ehacko@users.noreply.github.com>
2021-10-05 21:29:08 -0400

    Create Back_office.php

---
+++ Back_office.php
@@ -0,0 +1,33 @@
+<?php
+//   header("Cache-Control: max-age=2592000");
+//   $seconds_to_cache = 60*60*24*7;
+//   $ts = gmdate("D, d M Y H:i:s", time() + $seconds_to_cache) . " GMT";
+//   header("Expires: $ts");
+//   header("Pragma: cache");
+//   header("Cache-Control: max-age=$seconds_to_cache");
+#  include 'cache/top-cache.php';
+  include 'components/db.php';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <?php include 'components/head.php' ?>
+  </head>
+  <body>
+    <main>
+      <?php include 'components/header.php' ?>
+      <?php include 'components/nav.php' ?>
+      <div class="main">
+        <?php include 'components/about.php' ?>
+        <?php include 'components/machines.php' ?>
+        <?php include 'components/media.php' ?>
+      </div>
+      <?php include 'components/footer.php' ?>
+    </main>
+  </body>
+</html>
+
+<?php
+  #$query_str = null; $db_select = null;
+  #include 'cache/bottom-cache.php';
+?>
