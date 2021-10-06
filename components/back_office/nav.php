c67475606c55c9c3a10ece73c8ff0eeb64594d1b
Ehacko <49614514+Ehacko@users.noreply.github.com>
2021-10-05 21:34:36 -0400

    Create nav.php

---
+++ components/back_office/nav.php
@@ -0,0 +1,23 @@
+<div class="sticky-top">
+  <nav>
+    <div class="navbar navbar-expand-lg navbar-dark">
+      <span></span>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#about">A propos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#machines">Nos machines</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#media">Média</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</div>
