201af958bd2e91a29ba48a80c8d1f9a558dcff42
Ehacko <49614514+Ehacko@users.noreply.github.com>
2021-10-05 21:36:09 -0400

    Create main.php

---
+++ components/back_office/main.php
@@ -0,0 +1,8 @@
+<main>
+  <?php include 'components/nav.php' ?>
+  <div class="main">
+    <?php include 'components/about.php' ?>
+    <?php include 'components/machines.php' ?>
+    <?php include 'components/media.php' ?>
+  </div>
+</main>
